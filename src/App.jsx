fd05457a4844460bdda55d4dc9aba7194e4d91d0
Termux-Master93 <brisantur8034@gmail.com>
2023-02-08 23:01:23 -0500

    create and initial proyect technical test

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+function App() {
+  return (
+    <div className='page'>
+      <h2>Buscador de peñicuñas</h2>
+      <header>
+        <form>
+          <input type={'text'} placeholder="searchs" />
+          <button type='submit'>Search</button>
+        </form>
+      </header>
+      <main>
+        content pe
+      </main>
+
+    </div>
+  )
+}
+
+export default App
